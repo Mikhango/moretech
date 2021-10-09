16e616bc93d192458f78d54365a7f08e4aa4dce2
Mikhail Abulfat <abulfat.misha@yandex.ru>
2021-10-09 12:21:11 +0300

    added db requests

--- web_project/main.py
+++ web_project/main.py
@@ -1,43 +1,79 @@
-from flask import Flask, render_template, request, redirect, make_response, url_for
+from flask import Flask, render_template, request, redirect, make_response, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 from reg_form import RegForm, LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-
-def login_accept_function(form):
-    email = form.email.data
-    password = form.password.data
-
-    # print(email)
-    # print(password)
-    # здесь логика базы данных
-    # print("\nData received. Now redirecting ...")
-    return redirect(url_for('index'))
+logged = False
 
 
 application = Flask(__name__)
 
 application.config['SECRET_KEY'] = 'secret key'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+db = SQLAlchemy(application)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    surname = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'<Users {self.id}>'
+
 
 @application.route('/')
 def index():
     return render_template('index.html')
 
-@application.route('/registration', methods=['get', 'post'])
+@application.route('/register', methods=['get', 'post'])
 def register():
     form = RegForm()
-    return render_template('register.html', title='Register', form=form)
+    if form.validate_on_submit():
+
+        email = form.email.data
+        password = form.password.data
+        name = form.name.data
+        surname = form.surname.data
+
+        user = Users(name=name, surname=surname, email=email, password=generate_password_hash(password))
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect(url_for('login'))
+        except:
+            return 'Error'
+
+    return render_template('register.html', form=form)
 
 @application.route('/login', methods=['get', 'post'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        login_accept_function(form)
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
 
-    return render_template('login.html', form=form)
+        curs = db.cursor()
+        info = curs.execute('SELECT * FROM users WHERE email=? password=?', (email, generate_password_hash(password)))
+        if info.fetchone() is None:
+            return redirect(url_for('register'))
+        else:
+            return redirect(url_for('cabinet'))
+    else:
+        return render_template('login.html', title='Login', form=form)
+
+@application.route('/')
+def cabinet():
+    return render_template('cabinet.html')
 
 @application.route('/books/<genre>')
 def books(genre):
