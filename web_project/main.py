24e23b33a53562f3c8743ef01d4c58967725ce80
Mikhail Abulfat <abulfat.misha@yandex.ru>
2021-10-09 09:15:38 +0300

    fixed bugs and added new)

--- web_project/main.py
+++ web_project/main.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template, request, redirect, make_response
+from flask import Flask, render_template, request, redirect, make_response, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 from reg_form import RegForm, LoginForm
+
+
+
+def login_accept_function(form):
+    email = form.email.data
+    password = form.password.data
+
+    # print(email)
+    # print(password)
+    # здесь логика базы данных
+    # print("\nData received. Now redirecting ...")
+    return redirect(url_for('index'))
 
 
 application = Flask(__name__)
@@ -14,15 +26,18 @@ application.config['SECRET_KEY'] = 'secret key'
 def index():
     return render_template('index.html')
 
-@application.route('/registration')
+@application.route('/registration', methods=['get', 'post'])
 def register():
     form = RegForm()
     return render_template('register.html', title='Register', form=form)
 
-@application.route('/login')
+@application.route('/login', methods=['get', 'post'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Log in', form=form)
+    if form.validate_on_submit():
+        login_accept_function(form)
+
+    return render_template('login.html', form=form)
 
 @application.route('/books/<genre>')
 def books(genre):
